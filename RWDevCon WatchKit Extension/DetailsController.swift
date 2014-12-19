62ecca90e101b96a3810778cb349bb7c77eedd9c
Greg Heo <greg@gregheo.com>
2014-12-19 18:42:55 -0500

    Load data and show table rows

---
+++ RWDevCon WatchKit Extension/DetailsController.swift
@@ -0,0 +1,7 @@
+
+import Foundation
+import WatchKit
+
+class DetailsController: WKInterfaceController {
+
+}
--- RWDevCon WatchKit Extension/ScheduleController.swift
+++ RWDevCon WatchKit Extension/ScheduleController.swift
@@ -1,10 +1,11 @@
 
-import WatchKit
 import Foundation
+import CoreData
+import WatchKit
 
 class ScheduleController: WKInterfaceController {
-
   @IBOutlet weak var scheduleTable: WKInterfaceTable!
+  lazy var coreDataStack = CoreDataStack()
   
   override func awakeWithContext(context: AnyObject?) {
     super.awakeWithContext(context)
@@ -13,8 +14,25 @@ class ScheduleController: WKInterfaceController {
   }
 
   override func willActivate() {
-    // This method is called when watch view controller is about to be visible to user
     super.willActivate()
+
+    if let conferencePlist = NSBundle.mainBundle().URLForResource("RWDevCon2015", withExtension: "plist") {
+      Config.loadDataFromPlist(conferencePlist, context: coreDataStack.context)
+      coreDataStack.saveContext()
+    }
+
+    let fetch = NSFetchRequest(entityName: "Session")
+    fetch.predicate = NSPredicate(format: "active = %@", argumentArray: [true])
+    fetch.sortDescriptors = [NSSortDescriptor(key: "date", ascending: true), NSSortDescriptor(key: "track.trackId", ascending: true)]
+
+    if let results = coreDataStack.context.executeFetchRequest(fetch, error: nil) as? [Session] {
+      scheduleTable.setNumberOfRows(results.count, withRowType: "ScheduleRow")
+
+      for (index, session) in enumerate(results) {
+        let row = scheduleTable.rowControllerAtIndex(index) as ScheduleRow
+        row.rowLabel.setText(session.fullTitle)
+      }
+    }
   }
 
   override func didDeactivate() {
