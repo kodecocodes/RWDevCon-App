a4f42f8bb113768757540622365d03bc86b0d0d5
Greg Heo <greg@gregheo.com>
2014-12-19 18:50:48 -0500

    Session details

--- RWDevCon WatchKit Extension/DetailsController.swift
+++ RWDevCon WatchKit Extension/DetailsController.swift
@@ -4,4 +4,15 @@ import WatchKit
 
 class DetailsController: WKInterfaceController {
 
+  @IBOutlet weak var titleLabel: WKInterfaceLabel!
+  @IBOutlet weak var image: WKInterfaceImage!
+  @IBOutlet weak var detailsLabel: WKInterfaceLabel!
+
+  override func awakeWithContext(context: AnyObject?) {
+    if let session = context as? Session {
+      titleLabel.setText(session.fullTitle)
+      detailsLabel.setText("\(session.startDateTimeString)\n\(session.sessionDescription)")
+      image.setHidden(true)
+    }
+  }
 }
--- RWDevCon WatchKit Extension/ScheduleController.swift
+++ RWDevCon WatchKit Extension/ScheduleController.swift
@@ -6,15 +6,10 @@ import WatchKit
 class ScheduleController: WKInterfaceController {
   @IBOutlet weak var scheduleTable: WKInterfaceTable!
   lazy var coreDataStack = CoreDataStack()
+  var sessions = [Session]()
   
   override func awakeWithContext(context: AnyObject?) {
     super.awakeWithContext(context)
-
-    // Configure interface objects here.
-  }
-
-  override func willActivate() {
-    super.willActivate()
 
     if let conferencePlist = NSBundle.mainBundle().URLForResource("RWDevCon2015", withExtension: "plist") {
       Config.loadDataFromPlist(conferencePlist, context: coreDataStack.context)
@@ -26,6 +21,8 @@ class ScheduleController: WKInterfaceController {
     fetch.sortDescriptors = [NSSortDescriptor(key: "date", ascending: true), NSSortDescriptor(key: "track.trackId", ascending: true)]
 
     if let results = coreDataStack.context.executeFetchRequest(fetch, error: nil) as? [Session] {
+      sessions = results
+
       scheduleTable.setNumberOfRows(results.count, withRowType: "ScheduleRow")
 
       for (index, session) in enumerate(results) {
@@ -33,6 +30,16 @@ class ScheduleController: WKInterfaceController {
         row.rowLabel.setText(session.fullTitle)
       }
     }
+  }
+
+  override func willActivate() {
+    super.willActivate()
+  }
+
+  override func table(table: WKInterfaceTable, didSelectRowAtIndex rowIndex: Int) {
+    let session = sessions[rowIndex]
+
+    presentControllerWithName("DetailsController", context: session)
   }
 
   override func didDeactivate() {
