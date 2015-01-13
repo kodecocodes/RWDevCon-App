1f892089ea298ec091d4bc4820d7e2ae56fa25ba
Greg Heo <greg@gregheo.com>
2015-01-13 08:46:47 -0500

    Collection view cleanup

--- RWDevCon/ScheduleDataSource.swift
+++ RWDevCon/ScheduleDataSource.swift
@@ -11,10 +11,8 @@ import UIKit
 import CoreData
 
 typealias TableCellConfigurationBlock = (cell: ScheduleTableViewCell, indexPath: NSIndexPath, session: Session) -> ()
-typealias CellConfigurationBlock = (cell: ScheduleCell, indexPath: NSIndexPath, session: Session) -> ()
-typealias HeaderConfigurationBlock = (header: ScheduleHeader, indexPath: NSIndexPath, group: NSDictionary, kind: String) -> ()
 
-class ScheduleDataSource: NSObject, UICollectionViewDataSource {
+class ScheduleDataSource: NSObject {
   var coreDataStack: CoreDataStack!
 
   var startDate: NSDate?
@@ -28,9 +26,7 @@ class ScheduleDataSource: NSObject, UICollectionViewDataSource {
   let widthPerHour: CGFloat = 180
   let firstHour = 8
   
-  var cellConfigurationBlock: CellConfigurationBlock?
   var tableCellConfigurationBlock: TableCellConfigurationBlock?
-  var headerConfigurationBlock: HeaderConfigurationBlock?
 
   private var allSessions: [Session] {
     let fetch = NSFetchRequest(entityName: "Session")
@@ -55,12 +51,6 @@ class ScheduleDataSource: NSObject, UICollectionViewDataSource {
     var times = [String]()
 
     if favoritesOnly {
-//      for session in self.allSessions {
-//        let last = times.last
-//        if (last == nil) || (last != nil && last! != session.formatDate("EEEE")) {
-//          times.append(session.formatDate("EEEE"))
-//        }
-//      }
       times.append("Friday")
       times.append("Saturday")
     } else {
@@ -75,80 +65,10 @@ class ScheduleDataSource: NSObject, UICollectionViewDataSource {
     return times
   }
 
-  private let trackHeaderTitles = ["Beginner", "Intermediate", "Advanced"]
-  private let hourHeaderTitles = ["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00"]
-  
-  // MARK: UICollectionViewDataSource
-  
-  func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-    return distinctTimes.count
-  }
-  
-  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return arrayOfSessionsForSection(section).count;
-  }
-  
-  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ScheduleCell", forIndexPath: indexPath) as ScheduleCell
-    let session = sessionForIndexPath(indexPath)
-    if let configureBlock = cellConfigurationBlock {
-      configureBlock(cell: cell, indexPath: indexPath, session: session)
-    }
-    return cell
-  }
-  
-  func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
-    let header = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: "ScheduleHeader", forIndexPath: indexPath) as ScheduleHeader
-    let group = groupDictionaryForSection(indexPath.section)
-    if let configurationBlock = headerConfigurationBlock {
-      configurationBlock(header: header, indexPath: indexPath, group: group, kind: kind)
-    }
-    return header
-  }
-  
-  // MARK: Public Utilites
-  
-  internal func indexPathsOfHourHeaderViews() -> NSArray {
-    let indexPaths = NSMutableArray()
-    for item in 0..<numberOfHoursInSchedule {
-      let indexPath = NSIndexPath(forItem: item, inSection: 0)
-      indexPaths.addObject(indexPath)
-    }
-    return indexPaths
-  }
-  
-  internal func indexPathsOfScheduleItems() -> NSArray {
-    let indexPaths = NSMutableArray()
-    for section in 0..<distinctTimes.count {
-      let sessions = arrayOfSessionsForSection(section)
-      for index in 0..<sessions.count {
-          let indexPath = NSIndexPath(forItem: index, inSection: section)
-          indexPaths.addObject(indexPath)
-      }
-    }
-    return indexPaths
-  }
-  
-  internal func indexPathsOfTrackHeaderViews() -> NSArray {
-    let indexPaths = NSMutableArray()
-    for item in 0..<numberOfTracksInSchedule {
-      let indexPath = NSIndexPath(forItem: item, inSection: 0)
-      indexPaths.addObject(indexPath)
-    }
-    return indexPaths
-  }
-    
+
   internal func sessionForIndexPath(indexPath: NSIndexPath) -> Session {
     let sessions = arrayOfSessionsForSection(indexPath.section)
     return sessions[indexPath.row]
-  }
-  
-  internal func titleForHourHeaderViewAtIndexPath(indexPath: NSIndexPath) -> String {
-    return hourHeaderTitles[indexPath.item]
-  }
-  
-  internal func titleForTrackHeaderViewAtIndexPath(indexPath: NSIndexPath) -> String {
-    return trackHeaderTitles[indexPath.item]
   }
   
   // MARK: Private Utilities
