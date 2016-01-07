3247d241912c4e898841cf3fe5d91421f0954271
Mic Pringle <mic@razeware.com>
2016-01-07 13:46:41 +0000

    Adds watch app

---
+++ Watch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Watch Extension
+//
+//  Created by Mic Pringle on 07/01/2016.
+//  Copyright © 2016 Razeware LLC. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
